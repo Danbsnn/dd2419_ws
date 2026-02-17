c7a98bade54905d4af1ed0f94c9d374d6b3efef7
Daniel <157037600+Danbsnn@users.noreply.github.com>
2026-02-17 01:01:37 +0100

    .

--- src/mapping/mapping/grid_publisher.py
+++ src/mapping/mapping/grid_publisher.py
@@ -72,7 +72,6 @@ class GridPublisher(Node):
             marker.pose.position.y = float(oy)
             marker.pose.orientation.w = 1.0
 
-            # --- Visual Logic based on Type ---
             if obj_type == 'O':  # Object = Red cube
                 marker.type = Marker.CUBE
                 marker.scale.x = 0.1
@@ -97,14 +96,14 @@ class GridPublisher(Node):
 
             elif obj_type == 'S': # Start = blue sphere
                 marker.type = Marker.SPHERE
-                marker.scale.x = 0.15 # Slightly larger
+                marker.scale.x = 0.15
                 marker.scale.y = 0.1
                 marker.scale.z = 0.15
                 marker.pose.position.z = 0.0
                 marker.color.r = 0.0
                 marker.color.g = 0.0
                 marker.color.b = 1.0
-                marker.color.a = 0.8 # Slightly transparent
+                marker.color.a = 0.8 
 
             ma.markers.append(marker)
         
